6a6adad8856112ea7074bc1a385017a0fab8ecf9
Oleg Mihailik <mihailik@gmail.com>
2024-01-16 12:29:42 +0000

    Separate handling of Octokit, saner build.js

--- build.js
+++ build.js
@@ -29,11 +29,6 @@ async function build(mode) {
       'node:constants', 'node:buffer', 'node:querystring', 'node:events', 'node:fs', 'node:path', 'node:os',
       'node:crypto', 'node:util', 'node:stream', 'node:assert', 'node:tty', 'node:net', 'node:tls', 'node:http',
       'node:https', 'node:zlib', 'node:http2', 'node:perf_hooks', 'node:child_process', 'node:worker_threads',
-      'node:node:constants', 'node:node:buffer', 'node:node:querystring', 'node:node:events', 'node:node:fs',
-      'node:node:path', 'node:node:os', 'node:node:crypto', 'node:node:util', 'node:node:stream',
-      'node:node:assert', 'node:node:tty', 'node:node:net', 'node:node:tls', 'node:node:http', 'node:node:https',
-      'node:node:zlib', 'node:node:http2', 'node:node:perf_hooks', 'node:node:child_process',
-      'node:node:worker_threads',
 
       'ws'
     ],
