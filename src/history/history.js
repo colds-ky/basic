c131788bba27ce5329c9834ed26b47b69aa116f0
Oleg Mihailik <mihailik@gmail.com>
2024-06-27 20:01:02 +0100

    Try getting better coldsky imports.

--- src/history/history.js
+++ src/history/history.js
@@ -3,10 +3,19 @@
 import React from 'react';
 
 import { useNavigate, useParams } from 'react-router-dom';
+import { unwrapShortHandle } from 'coldsky';
 
 export function History() {
 
   let { handle } = useParams();
+
+  if (handle) {
+    console.log({
+      handle,
+      unwrap: unwrapShortHandle(handle),
+    });
+  }
+
   return (
     <div>
       History...
