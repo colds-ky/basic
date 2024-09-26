9f72c37fc97bcdc104014cf906bdbdc9c75db5ba
Oleg Mihailik <mihailik@gmail.com>
2024-09-26 21:21:37 +0100

    Reusable allQuoting.

--- package/data/capture-records/compact-post-quotes.js
+++ package/data/capture-records/compact-post-quotes.js
@@ -17,3 +17,27 @@ export function detectQuoting(url, quotes) {
     return addToArrayUnique(quotes, makeFeedUri(postUri.shortDID, postUri.postID));
 }
 
+/**
+ * @param {import('../../firehose').RepoRecord$Typed['app.bsky.feed.post']} record
+ */
+export function allQuoting(record) {
+  /** @type {string[] | undefined} */
+  let quoting;
+
+  if (record.embed?.external)
+    quoting = detectQuoting(
+      /** @type {import('@atproto/api').AppBskyEmbedExternal.Main['external']} */(record.embed.external).uri,
+      quoting);
+  // if (record.embed)
+
+  if (record.facets?.length) {
+    for (const facet of record.facets) {
+      if (facet.features?.length) {
+        for (const feat of facet.features)
+          quoting = detectQuoting(/** @type {*} */(feat.uri), quoting);
+      }
+    }
+  }
+
+  return quoting;
+}
